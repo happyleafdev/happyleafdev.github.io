bb1e39ccfbbeada959f73d10778397f5b98f57a9
Taylor Svendsen <taysvendsen@gmail.com>
2019-03-01 18:46:53 -0800

    refactored

---
+++ js/app.js
@@ -0,0 +1,11 @@
+// function openNav(){
+//   alert('bitch')
+//   document.getElementById('overlay').style.width="500px";
+//   document.getElementById('body-wrapper').style.width="50px";
+// };
+//
+// function closeNav(){
+//   alert('bitch')
+//   document.getElementById('overlay').style.width="50px";
+//   document.getElementById('body-wrapper').style.width="500px";
+// }
